7dc10b3b0973c74a967c8db1185f388ae7542445
gundammaster <alexchamp23@hotmail.com>
2012-02-01 19:54:08 -0500

    Update class1/labs/lpthw_ex2.py

---
+++ class1/labs/lpthw_ex2.py
@@ -0,0 +1,8 @@
+# a comment, this is so you can read your program later.
+# anything after the # is ignored by python.
+
+print "I could have done this." # and the comment afetr is ignored
+
+#you can also use a comment to "Disable" or comment out a piece of code
+# print "this won't run"
+print "this ill run."
